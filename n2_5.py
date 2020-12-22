c65b736f092d3f92edd8dd674018061dbff50590
sfd <kater167761@gmail.com>
2020-12-22 20:41:00 +0300

    reworked N5

--- n2_5.py
+++ n2_5.py
@@ -5,11 +5,12 @@ while True:
 	newNum = int(input("Введите новый элемент рейтинга >> "))
 	if raiting[0] < newNum:
 		raiting.insert(0, newNum)
-	elif raiting[len(raiting) - 1] > newNum:
+	elif raiting[len(raiting) - 1] >= newNum:
 		raiting.insert(len(raiting), newNum)
 	else:
 		for i in range(len(raiting) - 1):
-			if raiting[i + 1] <= newNum < raiting[i]:
+			if raiting[i] >= newNum > raiting[i + 1]:
 				raiting.insert(i + 1, newNum)
+				break
 
 	print(raiting)
